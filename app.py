3afc52b9210d074226252bf305b527d86d3aeac4
raflyaulya <auliarafli819@gmail.com>
2023-10-07 13:28:03 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+# from your_analysis_module import analyze_reviews                  # Ganti dengan modul analisis kamu
+from analysis_customer_reviews import format_airline_name           # Ganti dengan modul analisis kamu
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    airline_name = request.form['airline_name']
+    result = format_airline_name(airline_name)  # Panggil fungsi analisis Python kamu
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
