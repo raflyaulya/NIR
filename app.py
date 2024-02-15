a1bbf4c5af6606b93c0405c5049e867b6ca5866c
raflyaulya <auliarafli819@gmail.com>
2024-02-16 01:13:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for 
 from file_analyze import airline_func           # change ur analysis library
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -12,10 +14,11 @@ def index():
 def finalResult():
     if request.method == 'POST':
         res_airlineName = request.form['airlineName']
-        result = airline_func(res_airlineName)  
-        return render_template('finalResult.html', result=result)
-
+        result, plot = airline_func(res_airlineName)  
+        return render_template('finalResult.html', result=result,
+                               plot=plot)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # d:/ИПМКН/Семестр 5/НИР/NIR/app.py
