8930c31ba14a10979a6305b042f28e47409b7aa1
raflyaulya <auliarafli819@gmail.com>
2024-02-16 01:13:46 +0700

    Update file_analyze.py

--- file_analyze.py
+++ file_analyze.py
@@ -1,11 +1,26 @@
 import requests
+import string
+import re
 from bs4 import BeautifulSoup
 import random
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import io
+import base64
+# import seaborn as sns
+
+import nltk
+# nltk.download()
+from nltk.corpus import stopwords
+from textblob import TextBlob
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
 
-def airline_func(airline_name):
-    formatted_name = '-'.join(airline_name.split())
     # return formatted_name
     
 
@@ -19,6 +34,8 @@ def airline_func(airline_name):
 # print(output_name)  # Output: british-airways
 
 
+def airline_func(airline_name):
+    formatted_name = '-'.join(airline_name.split())
 # base_url = "https://www.airlinequality.com/airline-reviews/british-airways"
     base_url = f"https://www.airlinequality.com/airline-reviews/{formatted_name}"         # output_name
     pages = 10
@@ -53,56 +70,61 @@ def airline_func(airline_name):
     df.reviews = df.reviews.apply(lambda x: x.replace(unnecessary_word3, ''))
 
 
-    randcol = random.choice(df['reviews'])   # untuk mencari RANDOM REVIEWS
+    # randcol = random.choice(df['reviews'])   # untuk mencari RANDOM REVIEWS
     
-    return randcol   # print out RANDOM REVIEWS 
+    # return randcol   # print out RANDOM REVIEWS 
 
 
 # airline_name = input('enter the name of airlines: ')
 # res = airline_func(airline_name)
 # print(res)
 
-    # DATA ANALYSIS
-    # def analyze_sentiment(reviews):
-    #     tokens = nltk.word_tokenize(reviews)
-        
-    #     tagged_tokens = nltk.pos_tag(tokens)
 
-    #     lemmatized_words = []
-    #     lemmatizer = nltk.WordNetLemmatizer()
-    #     for word, tag in tagged_tokens:
-    #         tag = tag[0].lower() if tag[0].lower() in ['a', 'r', 'n', 'v'] else 'n'
-    #         lemmatized_words.append(lemmatizer.lemmatize(word, tag))
+    # ==============             DATA ANALYSIS       =================================
+    
+    def analyze_sentiment(reviews):
+        tokens = nltk.word_tokenize(reviews)
+            
+        tagged_tokens = nltk.pos_tag(tokens)
 
-    #     stopwords = nltk.corpus.stopwords.words('english')
-    #     clean_words = [word for word in lemmatized_words if word.lower() not in stopwords]
+        lemmatized_words = []    
+        lemmatizer = nltk.WordNetLemmatizer()
+        for word, tag in tagged_tokens:
+            tag = tag[0].lower() if tag[0].lower() in ['a', 'r', 'n', 'v'] else 'n'
+            lemmatized_words.append(lemmatizer.lemmatize(word, tag))    
 
-    #     clean_text = ' '.join(clean_words)
+        cust_stopwords = nltk.corpus.stopwords.words('english')
+        clean_words = [word for word in lemmatized_words if word.lower() not in cust_stopwords]
 
-    #     blob = TextBlob(clean_text)
-    #     polarity = blob.sentiment.polarity
-    #     subjectivity = blob.sentiment.subjectivity
+        clean_text = ' '.join(clean_words)
 
-    #     if polarity > 0:
-    #         sentiment = 'Positive'
-    #     elif polarity < 0:
-    #         sentiment = 'Negative'
-    #     else:
-    #         sentiment = 'Neutral'
+        blob = TextBlob(clean_text)
+        polarity = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
 
-    #     return sentiment, polarity, subjectivity
+        if polarity > 0:
+            sentiment = 'Positive'
+        elif polarity < 0:
+            sentiment = 'Negative'
+        else:
+            sentiment = 'Neutral'
 
-    # df[['sentiment', 'polarity', 'subjectivity']] = df['reviews'].apply(analyze_sentiment).apply(pd.Series)
+        return sentiment, polarity, subjectivity
+
+    df[['sentiment', 'polarity', 'subjectivity']] = df['reviews'].apply(analyze_sentiment).apply(pd.Series)
 
 
     # print('Please select which sentiment reviews you want to display')
     # review_option = input('positive / negative / neutral?\n')
-    # review_option = 'Positive'
+    review_option = 'Positive'        #===============================
 
-    # # if review_option == 'Positive':
-    # positive_reviews = df[df['sentiment'] == 'Positive']
-    # random_review = np.random.choice(positive_reviews['reviews'])
-    
+    if review_option == 'Positive':
+        positive_reviews = df[df['sentiment'] == 'Positive']
+    random_review = np.random.choice(positive_reviews['reviews'])
+
+    # positive_reviews = df[df['sentiment']== review_option]   ====================
+    # random_review = np.random.choice(positive_reviews['reviews']) ======================
+
     # return random_review
                                          
     # if review_option == 'Positive':
@@ -122,59 +144,84 @@ def airline_func(airline_name):
 #  ==================    POSITIVE    ==================
 # ==========              ANALYZING DATA         =====================
 # print('Please select which sentiment reviews you want to analyze')
-# review_option = 'Positive'
+    review_option = 'Positive'
 
-# sentiment_reviews = df[df['sentiment'] == review_option]
-# sentiment_reviews = sentiment_reviews['reviews'].tolist()
-# sentiment_reviews = ' '.join(sentiment_reviews)
-# text = sentiment_reviews
-# # text
-# text = text.lower()
-# text = re.sub(r'\d+','', text)
+    sentiment_reviews = df[df['sentiment'] == review_option]
+    sentiment_reviews = sentiment_reviews['reviews'].tolist()
+    sentiment_reviews = ' '.join(sentiment_reviews)
+    text = sentiment_reviews
+    # text
+    text = text.lower()
+    text = re.sub(r'\d+','', text)
 
-# text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.translate(str.maketrans('', '', string.punctuation))
 
-# tokens = [word for word in text.split()]
-# # nltk.download()
-# clean_tokens = tokens[:]
-# for token in tokens:
-#     if token in stopwords.words('english'):
-#         clean_tokens.remove(token)
+    tokens = [word for word in text.split()]
+    # nltk.download()
+    custom_stopwords = stopwords.words('english')
+    clean_tokens = tokens[:]
+    # custom_stopwords = set(stopwords.words('english'))
+    # clean_tokens = [token for token in tokens if token.lower() not in custom_stopwords]
 
-# freq = nltk.FreqDist(clean_tokens)
-# for key, val in freq.items():
-#     print(str(key), ':', str(val))
+    for token in tokens:
+        if token in custom_stopwords:   #words('english'):
+            clean_tokens.remove(token)
+
+    freq = nltk.FreqDist(clean_tokens)
+    for key, val in freq.items():
+        print(str(key), ':', str(val))
 
 
-# ==============   VISUALISASI DATA     =============
-# =================================================================
-# freq.plot(30)       
+    # # ==============   VISUALISASI DATA     =============
+    # # =================================================================
+    # freq_pict = freq.plot(30)       
 
-# lis = []
-# freq = nltk.FreqDist(clean_tokens)
-# for key in freq.items():
-#     lis.append(str(key[0]))
+    lis = []
+    freq = nltk.FreqDist(clean_tokens)
+    for key in freq.items():
+        lis.append(str(key[0]))
 
-# print(lis)
-# print('==================\n')
+    # print(lis)
+    # print('==================\n')
 
-# stopwords= set(STOPWORDS)
-# wordcloud = WordCloud(width=800, height=800,
-#                       background_color='white', stopwords=stopwords,
-#                       max_words=50,
-#                       min_font_size=10).generate(' '.join(lis))
 
-# plt.figure(figsize= (8,8), facecolor=None)
-# plt.imshow(wordcloud, interpolation='bilinear')
-# plt.axis('off')
-# plt.tight_layout(pad=0)
+    def visual_pict(lis):
+        stopwords= set(STOPWORDS)
+        wordcloud = WordCloud(width=800, height=800,
+                            background_color='white', stopwords=stopwords,
+                            max_words=50,
+                            min_font_size=10).generate(' '.join(lis))
 
-# plt.show()
+        plt.figure(figsize= (8,8), facecolor=None)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.tight_layout(pad=0)
+        show_pict = plt.show()
 
-# total_positive = df['sentiment'] == 'Positive'
-# total_negative = df['sentiment'] == 'Negative'
-# total_neutral = df['sentiment'] == 'Neutral'
+        return show_pict
+    
+    res_fin = visual_pict(lis)
 
-# print('total positive sentiments:', total_positive.sum())
-# print('total negative sentiments:', total_negative.sum())
-# print('total neutral sentiments:', total_neutral.sum())
+    buffer = io.BytesIO(res_fin)
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)  # rewind the buffer to start of content
+
+    # encode plot ke dalam base64
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png).decode()
+    buffer.close()
+
+    return graph, random_review
+    # return res_fin
+
+# name_airline = 'british airways'
+# final = airline_func(airline_name=name_airline)
+# print(final)
+
+    # total_positive = df['sentiment'] == 'Positive'
+    # total_negative = df['sentiment'] == 'Negative'
+    # total_neutral = df['sentiment'] == 'Neutral'
+
+    # print('total positive sentiments:', total_positive.sum())
+    # print('total negative sentiments:', total_negative.sum())
+    # print('total neutral sentiments:', total_neutral.sum())
