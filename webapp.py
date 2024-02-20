58506dd8c7cfc35874332f7070bd0ec8ab333482
raflyaulya <auliarafli819@gmail.com>
2024-02-20 19:18:01 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for 
+from file_analyze import airline_func
+import io
+import base64
+
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def hello_world():
-    return 'WILLIAM KONTOL'
+@app.route('/finalResult', methods=['POST'])
+def finalResult():
+    if request.method == 'POST':
+        res_airlineName = request.form['airlineName']
+        plots, result_positive, result_negative = airline_func(res_airlineName)  
+        return render_template('finalResult.html',plots=plots, 
+                               result_positive=result_positive, result_negative = result_negative)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0') #debug = TRUE
+    # d:/ИПМКН/Семестр 5/НИР/NIR/app.py
