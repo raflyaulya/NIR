f05355e7062d2180e9bc498ba24fe06d5619736d
raflyaulya <auliarafli819@gmail.com>
2024-03-24 21:48:01 +0700

    Update webapp11.py

--- webapp11.py
+++ webapp11.py
@@ -1,8 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
 
-@app.route('/')
+# @app.route('/')
 
-def hello_world():
-    return 'WILLIAM KONTOL'
+# def hello_world():
+#     return 'tesst 123S'
+
+def coba(test):
+
+    # test = 'bob'
+    lis = []
+
+    if test == 'aka':
+        lis.append('true')
+    elif test == 'bob':
+        lis.append('WRONG')
+    else:
+        lis.append('I D K')
+
+
+    return lis[0]
+
+test= input('enter coba: ')
+
+print(coba(test))
